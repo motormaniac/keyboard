f36c915c349c59123ea70cb6bc223d6f603d34d3
motormaniac <darrienlee626@gmail.com>
2025-12-15 16:38:55 -0500

    Finish practice words on dictionary sets

--- pysrc/words.py
+++ pysrc/words.py
@@ -1,20 +1,59 @@
 import sys
 sys.path.append(".")
+from random import shuffle
 
-CHARACTERS = ["e","a","s","i","t","n"]
+ADDED_CHARACTERS = [
+    ["e","a","s","i","t","n"],
+    ["r", "h"],
+    ["p", "u"],
+    ["l","f"],
+    ["d", "o"],
+    ["m", "c"],
+    ["y", "w"],
+    ["b", "g"],
+    ["v", "j"],
+    ["z", "k"],
+    ["x", "q"],
+]
 
-output_words = []
+WORD_MAX = 100 # maximum words to generate per line. -1 means unlimited
+current_characters:list[str] = []
+output_lines:list[str] = []
 
+word_list = []
 with open("data/words.txt", "r") as f:
-    # each word is separated by a newline
-    for word in f:
+    word_list = [word.strip() for word in f.readlines()]
+    shuffle(word_list)
+
+# each word is separated by a newline
+for new_characters in ADDED_CHARACTERS:
+    current_characters += new_characters
+    print(current_characters)
+    this_line:str = ""
+    word_count = 0
+    for word in word_list:
+        if not WORD_MAX == -1 and word_count >= WORD_MAX:
+            break
         word = word.strip()
-        valid_word = True
+        only_has_allowed_letters = True
+        # check that the word contains only allowed characters
         for char in word:
-            if char not in CHARACTERS:
-                valid_word = False
-        if valid_word:
-            output_words.append(word)
+            if char not in current_characters:
+                only_has_allowed_letters = False
+        if not only_has_allowed_letters:
+            continue
+        # check that at least one of the added chars exists
+        contains_new_letters = False
+        for char in new_characters:
+            if char in word:
+                contains_new_letters = True
+                break
+        if not contains_new_letters:
+            continue
+        this_line += f"'{word}', "
+        word_count += 1
+    output_lines.append(f"\n\t[{this_line}],")
+    print(word_count)
 
 with open("data/practice_words.txt", "w") as f:
-    f.write(" ".join(output_words))
+    f.write(f"[{"".join(output_lines)}\n]")
